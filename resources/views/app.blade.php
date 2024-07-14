e9525a161529189eb557ca825bdd94a136906341
Mate Pušić <mate@bjelopic.com>
2024-07-14 19:40:03 +0200

    fix font load?

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,13 @@
     <meta name="darkreader-lock">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet" />
      <link rel="icon" type="image/x-icon" href="/favicon.ico">
      <link rel="icon" type="image/x-icon" href="/favicon48.ico">
      <link rel="icon" type="image/x-icon" href="/favicon64.ico">
+
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     @vite('resources/js/app.js')
     @vite(['resources/css/app.css'])
