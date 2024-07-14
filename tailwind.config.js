86c79032418fff3f53debbc7aed37c63599fbdb4
Mate Pušić <mate@bjelopic.com>
2024-07-14 20:49:07 +0200

    fix font?

--- tailwind.config.js
+++ tailwind.config.js
@@ -108,7 +108,7 @@ const config = {
                 sm: "calc(var(--radius) - 4px)"
             },
             fontFamily: {
-                sans: [...fontFamily.sans]
+                sans: ["Noto Sans", ...fontFamily.sans]
             }
         }
     },
