c37d709dbd3876a8a53b25d26f1cea545c3f4fcb
Mate Pušić <mate@bjelopic.com>
2024-07-14 19:33:52 +0200

    exclude from darkreader

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
+    <meta name="darkreader-lock">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="preconnect" href="https://fonts.googleapis.com" />
