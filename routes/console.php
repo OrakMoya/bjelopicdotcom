9c416e8fdfac258e6f080d7dab7000d9a7541d24
Mate Pušić <mate@bjelopic.com>
2024-07-23 11:26:09 +0200

    schedule telescope prune

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('telescope:prune')->dailyAt('06:00');
-Schedule::command('telescope:prune')->hourlyAt(57);
