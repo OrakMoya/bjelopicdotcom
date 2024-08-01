83414a25e8f6e000851d92453a7bd0a675468932
Mate Pušić <mate@bjelopic.com>
2024-08-01 06:25:16 +0200

    fix temporary change

--- resources/js/Pages/Hero/Layout.svelte
+++ resources/js/Pages/Hero/Layout.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col h-screen font-sans">
     <div class=" bg-black border-b border-neutral-800 flex justify-center">
-        <span class="text-white drop-shadow-sm text-xs sm:text-sm p-2"
+        <span class="text-white drop-shadow-sm text-xs sm:text-sm p-2 text-center"
             >BjeloPIC je na kolektivnom godišnjem odmoru u razdoblju od 1.8. do
             13.8. Hvala na razumijevanju.</span
         >
