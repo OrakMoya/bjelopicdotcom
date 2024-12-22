5f2fe84e48436df288fa2c870468e35382cde26b
Mate Pušić <mate@bjelopic.com>
2024-12-22 15:02:40 +0100

    swap buttons

--- resources/js/Pages/Hero/HeroLinks.svelte
+++ resources/js/Pages/Hero/HeroLinks.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 my-1 text-sm sm:text-base items-center">
-    <a
-        href="mailto:info@bjelopic.com"
+    <Link
+        href="/gallery"
         class="px-4 py-2 rounded-xl bg-bjelopic-red-2 transition duration-300 text-white hover:bg-bjelopic-red-1 drop-shadow-md text-center text-base"
-        ><span class="drop-shadow"> Kontaktirajte nas</span></a
+        ><span class="drop-shadow">Naši radovi</span></Link
     >
     <div class="grid grid-cols-2 gap-4 sm:col-span-2">
-        <Link
-            href="/gallery"
+        <a
+            href="mailto:info@bjelopic.com"
             class="px-4 py-2 rounded-xl bg-black/50 drop-shadow-md transition duration-300 border border-neutral-700 text-white text-center hover:bg-neutral-800/75"
-            >Naši radovi</Link
+            >Kontaktirajte nas</a
         >
         <a
             href="https://blog.bjelopic.com"
