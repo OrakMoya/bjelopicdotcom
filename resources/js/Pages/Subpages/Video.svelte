a3cb137c135e7c19e2b875771b3444d56e0f58c1
Mate Pušić <mate@bjelopic.com>
2024-12-25 22:09:59 +0100

    styling changes

--- resources/js/Pages/Subpages/Video.svelte
+++ resources/js/Pages/Subpages/Video.svelte
@@ -12,21 +12,21 @@
 </svelte:head>
 
 <section class="p-4 max-w-screen-xl mx-auto mb-2">
-    <div class=" mb-2 md:mb-4">
+    <div class="mb-2">
         <div
             class="flex justify-evenly items-center transition-all duration-500 align-middle w-full gap-x-2 md:gap-x-4 mx-auto md:mt-2 max-w-screen-md"
         >
             <div
-                class="border-b border-white w-full h-min opacity-0 sm:opacity-100 transition-opacity duration-500"
+                class="border-b border-white w-full h-min transition-opacity duration-500"
             ></div>
             <span
-                class="whitespace-nowrap transition-all duration-500 text-lg sm:text-2xl relative md:bottom-[2px] font-semibold tracking-tight"
+                class="text-center min-w-fit transition-all duration-500 text-2xl relative md:bottom-[2px] font-semibold tracking-tight"
                 >{video.title} ({new Date(
                     video.publication_date,
                 ).getUTCFullYear()})</span
             >
             <div
-                class="border-b border-white w-full h-min opacity-0 sm:opacity-100 transition-opacity duration-500"
+                class="border-b border-white w-full h-min transition-opacity duration-500"
             ></div>
         </div>
         <div class="flex flex-col justify-center">
@@ -54,23 +54,23 @@
     <div class="mt-16">
         <div
             id="roles"
-            class="flex justify-evenly items-center transition-all duration-500 align-middle w-full gap-x-2 md:gap-x-4 mx-auto mb-4 md:mb-6 md:mt-2 max-w-screen-md"
+            class="flex justify-evenly items-center transition-all duration-500 align-middle w-full gap-x-2 md:gap-x-4 mx-auto mb-2 max-w-screen-md"
         >
             <div
-                class="border-b border-white w-full h-min opacity-0 sm:opacity-100 transition-opacity duration-500"
+                class="border-b border-neutral-500 w-full h-min transition-opacity duration-500"
             ></div>
             <span
-                class="whitespace-nowrap transition-all duration-500 text-lg sm:text-2xl relative md:bottom-[2px] font-semibold tracking-tight"
+                class="whitespace-nowrap text-center transition-all duration-500 text-lg sm:text-xl relative md:bottom-[2px] font-semibold tracking-tight"
                 >Naše uloge</span
             >
             <div
-                class="border-b border-white w-full h-min opacity-0 sm:opacity-100 transition-opacity duration-500"
+                class="border-b border-neutral-500 w-full h-min transition-opacity duration-500"
             ></div>
         </div>
         <div class="flex justify-center my-2 gap-2 flex-wrap">
             {#each video.roles as role}
                 <div
-                    class="bg-bjelopic-neutral-1 text-black rounded-sm text-base px-1 py-[0.5px]"
+                    class="bg-bjelopic-neutral-1 text-black rounded-sm text-sm sm:text-base transition-all duration-500 px-1 py-[0.5px]"
                 >
                     <span class="drop-shadow">{role}</span>
                 </div>
@@ -81,17 +81,17 @@
     <div class="{stills.length ? 'block' : 'hidden'} mt-16">
         <div
             id="stills"
-            class="flex justify-evenly items-center transition-all duration-500 align-middle w-full gap-x-2 md:gap-x-4 mx-auto mb-4 md:mb-6 md:mt-2 max-w-screen-md"
+            class="flex justify-evenly items-center transition-all duration-500 align-middle w-full gap-x-2 md:gap-x-4 mx-auto mb-2 max-w-screen-md"
         >
             <div
-                class="border-b border-white w-full h-min opacity-0 sm:opacity-100 transition-opacity duration-500"
+                class="border-b border-neutral-500 w-full h-min transition-opacity duration-500"
             ></div>
             <span
-                class="whitespace-nowrap transition-all duration-500 text-lg sm:text-2xl relative md:bottom-[2px] font-semibold tracking-tight"
+                class="whitespace-nowrap text-center transition-all duration-500 text-lg sm:text-xl relative md:bottom-[2px] font-semibold tracking-tight"
                 >Stillovi</span
             >
             <div
-                class="border-b border-white w-full h-min opacity-0 sm:opacity-100 transition-opacity duration-500"
+                class="border-b border-neutral-500 w-full h-min transition-opacity duration-500"
             ></div>
         </div>
         <div class="grid grid-cols-1 gap-8">
