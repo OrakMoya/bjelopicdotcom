a90a2b1ce2cc829f0082ae8a32700052b079208b
Mate Pušić <mate@bjelopic.com>
2025-03-01 19:07:16 +0100

    change time of sending birthday emails

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('telescope:prune')->dailyAt('06:00');
 Schedule::command('app:purge-expired-temporary-uploads')->everyMinute();
 Schedule::command('app:purge-invalid-temporary-uploads')->everyMinute();
-Schedule::command('app:send-birthday-emails')->dailyAt('00:05');
+Schedule::command('app:send-birthday-emails')->dailyAt('12:00');
